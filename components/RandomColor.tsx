f8e109f4408914e29d0a61ac42cb89e778c7768a
asterisktotle <barandajander.gmail.com>
2025-01-12 12:04:54 +0800

    randomColor done

--- components/RandomColor.tsx
+++ components/RandomColor.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 
 const RandomColor = () => {
 	const [typeColor, setTypeColor] = useState('Hex');
-	const [color, setColor] = useState('#b646de');
+	const [color, setColor] = useState('#000000');
 
 	const randomColorUtility = (length) => {
 		return Math.floor(Math.random() * length);
@@ -34,31 +34,39 @@ const RandomColor = () => {
 			const randomIndex = randomColorUtility(hexCode.length);
 			hexColor += hexCode[randomIndex];
 		}
-
 		setColor(hexColor);
-		console.log(hexColor);
 	};
 
 	const generateRgbClr = () => {
-		// const randomizer = Math.floor(Math.random() * )
+		const r = randomColorUtility(256);
+		const g = randomColorUtility(256);
+		const b = randomColorUtility(256);
+
+		setColor(`rgb(${r}, ${g}, ${b})`);
+		console.log('rgb click');
 	};
 
 	return (
-		<div className={`flex flex-col items-center p-2 h-2/4 gap-3 bg-[${color}]`}>
+		<div
+			className="flex flex-col items-center justify-center p-2 h-2/4 gap-3"
+			style={{ backgroundColor: color }}
+		>
 			<button
-				className="bg-green-500 p-3 rounded-md"
+				className=" mix-blend-plus-darker bg-green-500 p-3 rounded-md"
 				onClick={() => setTypeColor('RGB')}
 			>
 				Random RGB
 			</button>
 			<button
-				className="bg-blue-700 p-3 rounded-md"
+				className="mix-blend-plus-darker bg-blue-700 p-3 rounded-md"
 				onClick={() => setTypeColor('Hex')}
 			>
 				Random Hex
 			</button>
 			<button
-				className="bg-green-700 p-3 rounded-md"
+				className={`p-3 rounded-md ${
+					typeColor === 'Hex' ? 'bg-blue-700' : 'bg-green-500'
+				}`}
 				onClick={
 					typeColor === 'Hex' ? () => generateHexClr() : () => generateRgbClr()
 				}
